13cd9cd99bcd1f0de9a0f28b9d48bb8732c69b86
Pamela Carriel <pjcarriel@espe.edu.ec>
2023-05-19 10:03:39 -0500

    Ejecución

---
+++ Pila.java
@@ -0,0 +1,16 @@
+import java.until.Stack;
+
+public class PilaExample {
+    puclic static void main(String[] args){
+        Stack<Integer> pila = new Stack<>();
+
+        pila.push(10);
+        pila.push(20);
+        pila.push(30);
+
+        while (!pila.empty()){
+            System.out.print(pila.peek() + " ");
+            pila.pop();
+        }
+    }
+}
